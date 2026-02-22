6f96900db5a7027616d996369f30ef34ed9e0d35
SachinSuanr <sunarsssssss123@gmail.comgit config --global user.email sunarsssssss123@gmail.com>
2026-02-22 21:04:08 +0545

    day 3

---
+++ day3_practice.py
@@ -0,0 +1,52 @@
+#task 1 
+
+num = int(input("Enter the number: "))
+
+if(num % 2 == 0):
+    print(f"{num} is an even number.")
+
+else:
+    print(f"{num} is an odd number.")
+    
+    
+#task 2 
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+if(num1 >= num2 and num1 >= num3):
+    print(f"{num1} is the largest number.")
+elif(num2 >= num1 and num2 >= num3):
+    print(f"{num2} is the largest number.")
+else:
+    print(f"{num3} is the largest number.")
+    
+
+#task 3
+ecount = 0
+ocount = 0
+while True:
+    number = int(input("Enter the number: "))
+    
+    if(number == 0):
+        break
+    
+    if (number % 2 == 0):
+        ecount += 1
+    else:
+        ocount += 1
+
+print(f"The total even number is {ecount}")
+print(f"The total odd number is {ocount}")
+
+
+#task 4
+
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(i , end="")
+        
+        
+    print()
