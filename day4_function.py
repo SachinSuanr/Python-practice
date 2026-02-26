00af92884418dd31a2059f6b0ec25d334a8688f2
SachinSuanr <sunarsssssss123@gmail.com>
2026-02-26 20:08:19 +0545

    second task

--- day4_function.py
+++ day4_function.py
@@ -24,3 +24,37 @@ print(f"The sum of numbers is:{add(num1,num2)}")
 print(f"The difference of numbers is: {subtract(num1,num2)}")
 print(f"The multiplication of numbers is: {multiply(num1,num2)}")
 print(f"The division of numbers is: {divide(num1,num2)}")
+
+
+#task 2
+
+def analyze_numbers():
+    total = 0
+    even = 0
+    odd = 0
+    total_sum = 0
+    while True:
+        num = int(input("Enter the number: "))
+        
+        if num == 0:
+            break
+        total += 1
+        total_sum += num
+        
+        if num % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    return total,even,odd,total_sum
+
+
+
+total,even,odd,total_sum = analyze_numbers()
+
+print(f"Total numbers are: {total}")
+print(f"Total even numbers are: {even}")
+print(f"Total odd numbers are: {odd}")
+print(f"The total sum of the numbers are: {total_sum}")
+            
+        
+    
